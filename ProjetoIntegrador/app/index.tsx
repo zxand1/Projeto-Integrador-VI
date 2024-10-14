bfcbeb1d3d5cc4eb6d794ca539f1a3af3cfc74df
Lucas Venancio <94983682+WebLucasDev@users.noreply.github.com>
2024-10-14 11:46:42 -0300

    Primeiro Commit
    Setup inicial do projeto para implementação das telas desenvolvidas no Figma.

---
+++ ProjetoIntegrador/app/index.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Teste</Text>
+    </View>
+  );
+}
